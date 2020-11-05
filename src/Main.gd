53a4ab654d3f0da33f52038d6e92aa0ab6cff213
Michael Bortlik <Michael.Bortlik@gmail.com>
2020-11-05 20:24:41 +0100

    added sound added background added hotkeys

--- src/Main.gd
+++ src/Main.gd
@@ -21,6 +21,9 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
+	$Sound/Music.stop()
+	$Sound/DeathSound.play()
 	
 func new_game():
 	score = 0
@@ -28,6 +31,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Sound/Music.play()
 
 
 func _on_StartTimer_timeout():
