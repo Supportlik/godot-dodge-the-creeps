af238283394178855272240234ac0e97e43e11dc
Michael Bortlik <Michael.Bortlik@gmail.com>
2020-11-05 21:03:20 +0100

    fixed signal bug finished v1.0.1

--- src/Main.gd
+++ src/Main.gd
@@ -16,14 +16,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-func game_over():
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-	$HUD.show_game_over()
-	get_tree().call_group("mobs", "queue_free")
-	$Sound/Music.stop()
-	$Sound/DeathSound.play()
 	
 func new_game():
 	score = 0
@@ -67,4 +59,9 @@ func _on_HUD_start_game():
 
 
 func _on_Player_hit():
-	game_over()
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
+	$Sound/Music.stop()
+	$Sound/DeathSound.play()
